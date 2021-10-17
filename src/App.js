1ce65e2fa5ca2cd9fae0a32e6c4ea18cef21af97
RabeyaEty01 <etyrabeya01@gmail.com>
2021-10-17 23:47:04 +0600

    not found page added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+import NotFound from './Pages/NotFound/NotFound';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import notfoundimg from '../../images/404.jpg'
+
+const NotFound = () => {
+    return (
+        <div>
+            <img className="my-5" src={notfoundimg} alt="" />
+            <h5>It's Look Like The Page You Are Looking For Is Not Found.</h5>
+            <Link to="/home">
+               <small className="btn btn-primary rounded-pill px-4"><i className="fas fa-arrow-circle-left text-white"></i> Go Back</small>
+            </Link>
+        </div>
+    );
+};
+
+export default NotFound;
